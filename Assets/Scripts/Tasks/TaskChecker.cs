8f872eb7552f99c032fcc6fa7a0ea3d65cb97454
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-10-08 00:18:39 +1100

    Loads Victory scene

--- Assets/Scripts/Tasks/TaskChecker.cs
+++ Assets/Scripts/Tasks/TaskChecker.cs
@@ -2,19 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TaskChecker : MonoBehaviour
 {
-    List<Task> taskList = new List<Task>();
-    
+    public List<Task> taskList = new List<Task>();
+
     TaskChecker()
     {
         // Placeholder tasks
         taskList.Add(new Task("Find out what our current working directory is", "pwd", "/root"));
         taskList.Add(new Task("Create a folder called miners in root folder", "mkdir miners", "/root"));
-        taskList.Add(new Task("Navigate into the miners folder", "cd miners", "/root"));
+        taskList.Add(new Task("Navigate into the miners folder", "cd miners", "/root/miners"));
         taskList.Add(new Task("Create a file called file.txt in the miners folder", "touch file.txt", "/root/miners"));
-        taskList.Add(new Task("Print 'rush'", "echo rush", "/root"));
+        taskList.Add(new Task("Print 'rush'", "echo rush", ""));
         taskList.Add(new Task("Print the names of each file in miners folder", "ls", "/root/miners"));
         taskList.Add(new Task("Delete file.txt", "rm file.txt", "/root/miners"));
         taskList.Add(new Task("Delete miners folder", "rmdir miners", "/root"));
@@ -24,7 +25,7 @@ public class TaskChecker : MonoBehaviour
     // called after a command is entered.
     public void CheckTask(string command, string directory)
     {
-        if (command == taskList[0].commandToRun && directory == taskList[0].workingDirectory)
+        if (command == taskList[0].commandToRun && (directory == taskList[0].workingDirectory || taskList[0].workingDirectory == ""))
         {
             OnTaskCompleted();
         }
@@ -43,8 +44,14 @@ public class TaskChecker : MonoBehaviour
             else
             {
                 taskList.RemoveAt(0);
+                AllTasksCompleted();
                 Debug.Log("ALL TASKS COMPLETE!");
             }
         }
+    }
+
+    void AllTasksCompleted()
+    {
+        SceneManager.LoadScene("VictoryScene", LoadSceneMode.Single);
     }
 }
